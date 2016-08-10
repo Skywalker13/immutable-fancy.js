9462d6c0361548b2c6e7b989c2577603e9993f72
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-08-10 17:48:05 +0200

    Add a test to set an array

--- test/index.spec.js
+++ test/index.spec.js
@@ -16,5 +16,9 @@ describe ('FancyImmutable API', function () {
     /* Set a simple value in an array */
     expected = {a: {b: {c: 0}, d: ['x', 'a', 'z']}};
     expect (obj.fancySet ('a.d[1]', 'a').toJS ()).to.be.eql (expected);
+
+    /* Set an array in an object */
+    expected = {a: {b: {c: ['a', 'b']}, d: ['x', 'y', 'z']}};
+    expect (obj.fancySet ('a.b.c', ['a', 'b']).toJS ()).to.be.eql (expected);
   });
 });
