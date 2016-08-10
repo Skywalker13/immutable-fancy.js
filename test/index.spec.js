6a6b27e9c19f6796fe98831cc3ca423b583f3888
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-08-10 17:47:48 +0200

    Add comments

--- test/index.spec.js
+++ test/index.spec.js
@@ -9,9 +9,11 @@ describe ('FancyImmutable API', function () {
     let expected;
     const obj = fromJS ({a: {b: {c: 0}, d: ['x', 'y', 'z']}});
 
+    /* Set a simple value in an object */
     expected = {a: {b: {c: 1}, d: ['x', 'y', 'z']}};
     expect (obj.fancySet ('a.b.c', 1).toJS ()).to.be.eql (expected);
 
+    /* Set a simple value in an array */
     expected = {a: {b: {c: 0}, d: ['x', 'a', 'z']}};
     expect (obj.fancySet ('a.d[1]', 'a').toJS ()).to.be.eql (expected);
   });
