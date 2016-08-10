50a75adfa57e498c4c15285bdb445b770536c69c
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-08-10 17:48:47 +0200

    Add tests for fancyGet

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,9 +5,10 @@ const {fromJS} = require ('../lib/index.js');
 
 
 describe ('FancyImmutable API', function () {
+  const obj = fromJS ({a: {b: {c: 0}, d: ['x', 'y', 'z']}});
+
   it ('#fancySet', function () {
     let expected;
-    const obj = fromJS ({a: {b: {c: 0}, d: ['x', 'y', 'z']}});
 
     /* Set a simple value in an object */
     expected = {a: {b: {c: 1}, d: ['x', 'y', 'z']}};
@@ -20,5 +21,21 @@ describe ('FancyImmutable API', function () {
     /* Set an array in an object */
     expected = {a: {b: {c: ['a', 'b']}, d: ['x', 'y', 'z']}};
     expect (obj.fancySet ('a.b.c', ['a', 'b']).toJS ()).to.be.eql (expected);
+  });
+
+  it ('#fancyGet', function () {
+    let expected;
+
+    /* Get a simple value from an object */
+    expected = 0;
+    expect (obj.fancyGet ('a.b.c')).to.be.eql (expected);
+
+    /* Get a simple value from an array */
+    expected = 'y';
+    expect (obj.fancyGet ('a.d[1]')).to.be.eql (expected);
+
+    /* Get a List from an object */
+    expected = fromJS (['x', 'y', 'z']);
+    expect (obj.fancyGet ('a.d')).to.be.eql (expected);
   });
 });
